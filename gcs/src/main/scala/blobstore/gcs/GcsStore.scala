1ceb1d3c63b14f0e8a547640015c8d716c39581f
Victor Gafiatulin <gafiatulin@gmail.com>
2020-03-19 13:41:41 +0100

    implement putRotate pipe for gcs

--- gcs/src/main/scala/blobstore/gcs/GcsStore.scala
+++ gcs/src/main/scala/blobstore/gcs/GcsStore.scala
@@ -1,12 +1,13 @@
-package blobstore.gcs
+package blobstore
+package gcs
 
+import java.io.OutputStream
 import java.nio.channels.Channels
 import java.time.Instant
 import java.util.Date
 
-import blobstore.{Path, Store}
-import cats.effect.{Blocker, ContextShift, Sync}
-import cats.syntax.applicative._
+import cats.effect.{Blocker, Concurrent, ContextShift, Resource}
+import cats.syntax.all._
 import com.google.api.gax.paging.Page
 import com.google.cloud.storage.{Acl, Blob, BlobId, BlobInfo, Storage, StorageException}
 import com.google.cloud.storage.Storage.{BlobListOption, CopyRequest}
@@ -15,29 +16,28 @@ import fs2.{Chunk, Pipe, Stream}
 import scala.jdk.CollectionConverters._
 
 final class GcsStore[F[_]](storage: Storage, blocker: Blocker, acls: List[Acl] = Nil)(
-  implicit F: Sync[F],
+  implicit F: Concurrent[F],
   CS: ContextShift[F]
 ) extends Store[F] {
 
-  private def _chunk(pg: Page[Blob]): Chunk[Path] = {
-    val (dirs, files) = pg.getValues.asScala.toSeq.partition(_.isDirectory)
-    val dirPaths = Chunk.seq(
-      dirs.map(b =>
-        Path(root = b.getBucket, key = b.getName.stripSuffix("/"), size = None, isDir = true, lastModified = None)
+  def list(path: Path): Stream[F, Path] = {
+    def _chunk(pg: Page[Blob]): Chunk[Path] = {
+      val (dirs, files) = pg.getValues.asScala.toSeq.partition(_.isDirectory)
+      val dirPaths = Chunk.seq(
+        dirs.map(b =>
+          Path(root = b.getBucket, key = b.getName.stripSuffix("/"), size = None, isDir = true, lastModified = None)
+        )
       )
-    )
-    val filePaths = Chunk.seq(files.map {
-      b =>
+      val filePaths = Chunk.seq(files.map { b =>
         val size = Option(b.getSize: java.lang.Long).map(_.toLong) // Prevent throwing NPE (see https://github.com/scala/bug/issues/9634)
         val lastModified = Option(b.getUpdateTime: java.lang.Long).map(millis =>
           Date.from(Instant.ofEpochMilli(millis))
         ) // Prevent throwing NPE (see https://github.com/scala/bug/issues/9634)
         Path(b.getBucket, key = b.getName, size = size, isDir = false, lastModified = lastModified)
-    })
-    Chunk.concat(List(dirPaths, filePaths))
-  }
+      })
+      Chunk.concat(List(dirPaths, filePaths))
+    }
 
-  def list(path: Path): fs2.Stream[F, Path] = {
     Stream.unfoldChunkEval[F, () => Option[Page[Blob]], Path] { () =>
       Some(storage.list(path.root, BlobListOption.currentDirectory(), BlobListOption.prefix(path.key)))
     } { getPage =>
@@ -53,7 +53,7 @@ final class GcsStore[F[_]](storage: Storage, blocker: Blocker, acls: List[Acl] =
     }
   }
 
-  def get(path: Path, chunkSize: Int): fs2.Stream[F, Byte] = {
+  def get(path: Path, chunkSize: Int): Stream[F, Byte] = {
     val is = blocker.delay {
       Option(storage.get(path.root, path.key)).map(blob => Channels.newInputStream(blob.reader()))
     }
@@ -64,28 +64,36 @@ final class GcsStore[F[_]](storage: Storage, blocker: Blocker, acls: List[Acl] =
     }
   }
 
-  def put(path: Path): Pipe[F, Byte, Unit] = {
-    val fos = Sync[F].delay {
-      val builder = {
-        val b = BlobInfo.newBuilder(path.root, path.key)
-        if (acls.nonEmpty) b.setAcl(acls.asJava) else b
-      }
-      val blobInfo = builder.build()
-      val writer   = storage.writer(blobInfo)
-      Channels.newOutputStream(writer)
-    }
-    fs2.io.writeOutputStream(fos, blocker, closeAfterUse = true)
-  }
+  def put(path: Path): Pipe[F, Byte, Unit] =
+    fs2.io.writeOutputStream(newOutputStream(path), blocker, closeAfterUse = true)
 
-  def move(src: Path, dst: Path): F[Unit] = F.productR(copy(src, dst))(remove(src))
+  def move(src: Path, dst: Path): F[Unit] = copy(src, dst) *> remove(src)
 
   def copy(src: Path, dst: Path): F[Unit] = {
     val req = CopyRequest.newBuilder().setSource(src.root, src.key).setTarget(BlobId.of(dst.root, dst.key)).build()
-    F.void(blocker.delay(storage.copy(req).getResult))
+    blocker.delay(storage.copy(req).getResult).void
   }
 
   def remove(path: Path): F[Unit] =
-    F.void(blocker.delay(storage.delete(path.root, path.key)))
+    blocker.delay(storage.delete(path.root, path.key)).void
+
+  def putRotate(computePath: F[Path], limit: Long): Pipe[F, Byte, Unit] = {
+    val openNewFile: Resource[F, OutputStream] =
+      Resource.make(computePath.flatMap(newOutputStream))(os => blocker.delay(os.close()))
+
+    putRotateBase(limit, openNewFile)(os => bytes => blocker.delay(os.write(bytes)))
+  }
+
+  private def newOutputStream(path: Path): F[OutputStream] = F.delay {
+    val builder = {
+      val b = BlobInfo.newBuilder(path.root, path.key)
+      if (acls.nonEmpty) b.setAcl(acls.asJava) else b
+    }
+    val blobInfo = builder.build()
+    val writer   = storage.writer(blobInfo)
+    Channels.newOutputStream(writer)
+  }
+
 }
 
 object GcsStore {
@@ -93,5 +101,5 @@ object GcsStore {
     storage: Storage,
     blocker: Blocker,
     acls: List[Acl]
-  )(implicit F: Sync[F], CS: ContextShift[F]): GcsStore[F] = new GcsStore(storage, blocker, acls)
+  )(implicit F: Concurrent[F], CS: ContextShift[F]): GcsStore[F] = new GcsStore(storage, blocker, acls)
 }
