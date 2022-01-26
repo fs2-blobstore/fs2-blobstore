55abcf5116492ba90a90a7229b89e405e2c2a3dd
Scala Steward <me@scala-steward.org>
2022-01-27 00:28:09 +0100

    Update scalacheck-1-15 to 3.2.11.0

--- project/Tests.scala
+++ project/Tests.scala
@@ -7,7 +7,7 @@ object Tests extends AutoPlugin {
   override def buildSettings: Seq[Def.Setting[_]] = Seq(
     libraryDependencies ++= Seq(
       "org.scalatest"     %% "scalatest"       % "3.2.10"   % Test,
-      "org.scalatestplus" %% "scalacheck-1-15" % "3.2.10.0" % Test
+      "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test
     )
   )
 
