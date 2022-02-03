f259293fc85b0db4bf4c150d4941506bc3b8a685
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-03 14:43:46 +0100

    Update slf4j-simple to 1.7.35 (#603)

--- project/Logging.scala
+++ project/Logging.scala
@@ -8,6 +8,6 @@ object Logging extends AutoPlugin {
   override def requires = JvmPlugin
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
-    libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.33" % Test
+    libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.35" % Test
   )
 }
