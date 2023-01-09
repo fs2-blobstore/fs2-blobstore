b04e4dc032ee465201ac51e09effc16a0537ead2
Scala Steward <scala_steward@virtuslab.com>
2023-01-09 17:22:00 +0000

    Update scalatest to 3.2.15

--- project/Tests.scala
+++ project/Tests.scala
@@ -6,7 +6,7 @@ object Tests extends AutoPlugin {
 
   override def buildSettings: Seq[Def.Setting[_]] = Seq(
     libraryDependencies ++= Seq(
-      "org.scalatest"     %% "scalatest"       % "3.2.14"   % Test,
+      "org.scalatest"     %% "scalatest"       % "3.2.15"   % Test,
       "org.scalatestplus" %% "scalacheck-1-17" % "3.2.14.0" % Test
     )
   )
