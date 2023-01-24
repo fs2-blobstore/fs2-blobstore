20e497e9a7302a2f8c1cccfcbe4ca7016d03d3b7
Victor <gafiatulin@gmail.com>
2023-01-24 16:40:43 +0100

    Update Scalafix.scala

--- project/Scalafix.scala
+++ project/Scalafix.scala
@@ -2,7 +2,7 @@ import sbt.{Def, _}
 import sbt.Keys._
 import scalafix.sbt.ScalafixPlugin
 import scalafix.sbt.ScalafixPlugin.autoImport._
-import _root_.io.github.davidgregory084.{ScalaVersion, TpolecatPlugin, ScalacOption}
+import _root_.io.github.davidgregory084.{ScalaVersion, ScalacOption, TpolecatPlugin}
 import _root_.io.github.davidgregory084.TpolecatPlugin.autoImport._
 
 object Scalafix extends AutoPlugin {
@@ -11,9 +11,16 @@ object Scalafix extends AutoPlugin {
   override def requires = ScalafixPlugin && TpolecatPlugin
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
-    tpolecatScalacOptions ~= { _ ++ List(
-        ScalacOptions.privateOption("rangepos", version => version.isBetween(ScalaVersion.V2_12_0, ScalaVersion.V3_0_0)),
-        ScalacOption("-P:semanticdb:synthetics:on", version => version.isBetween(ScalaVersion.V2_12_0, ScalaVersion.V3_0_0))
+    tpolecatScalacOptions ~= {
+      _ ++ List(
+        ScalacOptions.privateOption(
+          "rangepos",
+          version => version.isBetween(ScalaVersion.V2_12_0, ScalaVersion.V3_0_0)
+        ),
+        ScalacOption(
+          "-P:semanticdb:synthetics:on",
+          version => version.isBetween(ScalaVersion.V2_12_0, ScalaVersion.V3_0_0)
+        )
       )
     }
   )
