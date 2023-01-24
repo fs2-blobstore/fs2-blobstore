96e04324c11b590979e89510f89a8f29783d614a
Victor <gafiatulin@gmail.com>
2023-01-24 13:43:24 +0100

    fix: type

--- s3/src/test/scala/blobstore/s3/S3StoreIntegrationTest.scala
+++ s3/src/test/scala/blobstore/s3/S3StoreIntegrationTest.scala
@@ -37,7 +37,7 @@ class S3StoreIntegrationTest extends AbstractS3StoreTest {
     .httpClient(httpClient)
     .build()
 
-  lazy val crtAsyncClient: S3CrtAsyncClient = S3CrtAsyncClient.builder().region(region).build()
+  lazy val crtAsyncClient: S3AsyncClient = S3CrtAsyncClient.builder().region(region).build()
 
   override def afterAll(): Unit = {
     store.remove(Url(scheme, authority, testRunRoot), recursive = true)
